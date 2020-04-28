012b5b124fd4d1bf9974087ac16e74543c61764c
walson <walson.wu@inmobi.com>
2020-04-28 19:05:33 +0800

    fix bug

--- downloadProductVideo.js
+++ downloadProductVideo.js
@@ -19,6 +19,10 @@ async function downloadVideo (url) {
       // 避免一个文件夹下文件过多，根据文件名分路径
       const str1 = name.substr(0,1);
       const str2 = name.substr(1,1);
+      await Promise.all([
+        makeDir(`videos/${str1}`),
+        makeDir(`videos/${str1}/${str2}`),
+      ]);
       const savePath = path.resolve(__dirname, `videos/${str1}/${str2}`, name);
       log.info(`保存路径: ${savePath}`);
       const writer = fs.createWriteStream(savePath);
