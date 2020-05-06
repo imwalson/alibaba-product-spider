e630641d76f30d5a8d258a66b4e184ff2a8ff297
walson <walson.wu@inmobi.com>
2020-05-06 11:47:03 +0800

    update

--- validVideoFilter.js
+++ validVideoFilter.js
@@ -409,4 +409,4 @@ async function exportValidVideos(listUrl, currency) {
 }
 
 
-exportValidVideos('https://www.alibaba.com/catalog/razor_cid100001042', 'SAR');
+exportValidVideos('https://www.alibaba.com/catalog/razor_cid100001042', 'EGP');
