8348c1b2faf7ba5f58099d867ebfffeffa2545a0
walson <walson.wu@inmobi.com>
2020-05-25 13:53:45 +0800

    address bind 0.0.0.0

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get('/api/category_by_pid/:pid', async function(req, res){
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port, '0.0.0.0');
 
 module.exports = app;
