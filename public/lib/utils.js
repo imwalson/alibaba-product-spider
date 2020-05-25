2876212f41971d74fb63e25e3f13d1462aeb7fe1
walson <walson.wu@inmobi.com>
2020-05-25 14:12:30 +0800

    change gitignore

---
+++ public/lib/utils.js
@@ -0,0 +1,37 @@
+function getQueryString(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  var reg_rewrite = new RegExp("(^|/)" + name + "/([^/]*)(/|$)", "i");
+  var r = window.location.search.substr(1).match(reg);
+  var q = window.location.pathname.substr(1).match(reg_rewrite);
+  if(r != null){
+    return unescape(r[2]);
+  }else if(q != null){
+    return unescape(q[2]);
+  }else{
+    return null;
+  }
+}
+
+/* 
+* url 目标url 
+* arg 需要替换的参数名称 
+* arg_val 替换后的参数的值 
+* return url 参数替换后的url 
+*/ 
+function changeURLArg(url, arg, arg_val) {
+  var pattern = arg + '=([^&]*)';
+  var replaceText = arg + '=' + arg_val;
+  if (url.match(pattern)) {
+    var tmp = '/(' + arg + '=)([^&]*)/gi';
+    tmp = url.replace(eval(tmp), replaceText);
+    return tmp;
+  } else {
+    if (url.match('[\?]')) {
+      return url + '&' + replaceText;
+    } else {
+      return url + '?' + replaceText;
+    }
+  }
+  return url + '\n' + arg + '\n' + arg_val;
+}
+
