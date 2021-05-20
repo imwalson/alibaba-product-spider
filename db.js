4a7286c9605f5a82edf9d6c02550e5d0d25452c4
imwalson <imwalson@163.com>
2021-05-20 11:15:20 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoist = require('mongoist');
 
-const DB_URL = 'mongodb://psotest:pso123@47.111.1.218:27017/alibaba_spider';
+const DB_URL = 'alibaba_spider'; // replace with your mongodb url
 const db = mongoist(DB_URL);
 
 module.exports = db;
